e86f0559de1f95200fd0292661e2607a512b404a
Alper Balbay <itsc0508@gmail.com>
2023-03-19 23:50:47 +0300

    final decision: self-improvement sells, there aren't any useful insight in this shit. stopped development

---
+++ medium_scraper/utils.py
@@ -0,0 +1,6 @@
+import requests
+import os
+
+def check_archive(url):
+    r = requests.get(os.path.join(url, "archive"))
+    return not (("PAGE NOT FOUND" in r.text) and ("404" in r.text))
---
+++ pipeline.py
@@ -0,0 +1,45 @@
+from medium_scraper import ProfileScraper, MediumScraper
+
+import pandas as pd
+import os
+
+from dotenv import load_dotenv
+
+
+def extract(urls):
+    scraper = ProfileScraper()
+    dataset = {}
+    for url in urls:
+        print("Scraping url:", url)
+        data = scraper.extract(url)
+        dataset[url] = data
+
+
+    data = []
+    for author_url, posts in dataset.items():
+        for post in posts:
+            post['author_url'] = author_url
+            data.append(post)
+
+    dataframe = pd.DataFrame(data)
+
+    dataset_name = "raw_dataset.csv"
+
+    dataframe.to_csv(os.path.join(DATASET_PATH, dataset_name), index=False)
+
+    return dataframe
+
+
+
+if __name__=="__main__":
+
+    load_dotenv()
+    DATASET_PATH = os.getenv("DATASET_PATH")
+
+
+    with open("authors.txt", "r") as f:
+        author_urls = f.readlines()
+
+    author_urls = list(map(lambda s: s.strip().strip("\n"), author_urls))
+
+    extract(author_urls)
