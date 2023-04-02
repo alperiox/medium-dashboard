b240d972c7298f9f6928f078972d3760b9cc32b0
Alper Balbay <itsc0508@gmail.com>
2023-04-02 21:44:09 +0300

    updated the dashboard starting application

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from dash import Dash, html, dcc, Input, Output, ctx
+# dash imports for the dashboard app
 import dash
-# dash templates
 import dash_bootstrap_components as dbc
+from dash import Dash, html
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.themes.LUX], use_pages=True)
+app = Dash(
+    __name__,
+    external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.themes.LUX],
+    use_pages=True,
+)
 
 navbar = dbc.NavbarSimple(
     children=[
-        dbc.NavItem(dbc.NavLink("Authors", href="http://localhost:8050/")),
+        dbc.NavItem(dbc.NavLink("Home", href="http://localhost:8050")),
+        dbc.NavItem(dbc.NavLink("Authors", href="http://localhost:8050/authors")),
         dbc.NavItem(dbc.NavLink("Publications", href="http://localhost:8050/publications")),
     ],
     brand="Medium Dashboard",
@@ -16,10 +21,13 @@ navbar = dbc.NavbarSimple(
     dark=True,
 )
 
-app.layout = html.Div([
-    navbar,
-    dash.page_container,
-], className="content")
+app.layout = html.Div(
+    [
+        navbar,
+        dash.page_container,
+    ],
+    className="content",
+)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
